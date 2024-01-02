47479704d8569b82d308c90cfaae280bdbd25789
Zhuangzhuang Dai <daiz1@mailhub.aston.ac.uk>
2024-01-02 12:11:49 +0000

    Done preprocessor

--- preprocess.py
+++ preprocess.py
@@ -3,75 +3,19 @@ from os.path import join
 import numpy as np
 import pandas as pd
 import pickle
+import openpyxl
 import json
 import datetime
 from datetime import timedelta
 from functools import reduce
 import matplotlib.pyplot as plt
-
-
-T_TOLERANCE = 3.0
-GRAD_THRES = -0.10
-
-
-def calculate_precision(tp, fp):
-    return tp / (tp + fp)
-
-
-def calculate_recall(tp, fn):
-    return tp / (tp + fn)
-
-
-def calculate_f1_score(precision, recall):
-    return 2 * (precision * recall) / (precision + recall)
-
-
-def calculate_metrics(predictions, ground_truth, time_window=T_TOLERANCE):
-    true_positives = 0
-    false_positives = 0
-    true_negatives = 0
-    false_negatives = 0
-
-    for prediction in predictions:
-        matched = False
-        for truth in ground_truth:
-            diff_t = abs(prediction - truth)
-            if diff_t <= time_window:
-                # Found a match within the time window
-                true_positives += 1
-                matched = True
-                break
-
-        if not matched:
-            # Prediction did not match any ground-truth event within the time window
-            false_positives += 1
-
-    # Calculation for TN and FN would depend on your specific scenario
-    for truth in ground_truth:
-        matched = False
-        for prediction in predictions:
-            diff_t = abs(prediction - truth)
-            if diff_t <= time_window:
-                # Found a match within the time window
-                # it is NOT a FN
-                matched = True
-                break
-
-        if not matched:
-            false_negatives += 1
-
-    # Calculate Precision, Recall, F1-Score using TP, FP, TN, FN per sample
-    #precision = calculate_precision(true_positives, false_positives)
-    #recall = calculate_recall(true_positives, false_negatives)
-    #F1 = calculate_f1_score(precision, recall)
-    # return precision, recall, F1
-    return true_positives, false_positives, false_negatives
+import argparse
 
 
 # Data loader class #
 class Preprocessor(object):
     def __init__(self, sheets_to_load=None, src_dir=None,
-                 pupil_lst=["fixations.csv", "blinks.csv", "head_pose_tracker_poses.csv"]):
+                 pupil_lst=["fixations.csv", "blinks.csv", "head_pose_tracker_poses.csv", "surface_events.csv", "Annotations.xlsx"]):
         if sheets_to_load is not None:
             assert isinstance(sheets_to_load, list)
             self.sheets_to_load = sheets_to_load
@@ -83,7 +27,7 @@ class Preprocessor(object):
         self.FIXATION_CONF_THRES = 0.7
         self.src_dir = src_dir
         # Annotations
-        self.anno_dict = pd.read_excel("./Annotations.xlsx", sheet_name=sheets_to_load)
+        #self.anno_dict = pd.read_excel("./Annotations.xlsx", sheet_name=sheets_to_load)
         # list of Pupil features of interest
         self.pupil_lst = pupil_lst  # "gaze_positions.csv"
 
@@ -93,7 +37,7 @@ class Preprocessor(object):
 
     def resample_data(self, sample):
 
-        with open(join(self.src_dir, sample, 'Pupil', "info.player.json")) as f:
+        with open(join(self.src_dir, 'data', sample, 'Pupil', "info.player.json")) as f:
             Info = json.load(f)
             start_time = Info['start_time_system_s']
             dt_start = datetime.datetime.fromtimestamp(start_time)
@@ -106,7 +50,7 @@ class Preprocessor(object):
             # TODO: need a common start_t=0 to 300 as shared indexed new_timestamp
             if modality == 'head_pose_tracker_poses.csv':
 
-                Head = pd.read_csv(join(self.src_dir, sample, 'Pupil', modality))
+                Head = pd.read_csv(join(self.src_dir, 'data', sample, 'Pupil', modality))
                 # Resample timestamps to the desired frequency
                 Head['new_timestamp'] = dt_start + pd.to_timedelta(Head['timestamp'], unit='S')
 
@@ -117,7 +61,7 @@ class Preprocessor(object):
                 #print(resampled_Head.head())
 
             elif modality == 'blinks.csv':
-                Blin = pd.read_csv(join(self.src_dir, sample, 'Pupil', modality))
+                Blin = pd.read_csv(join(self.src_dir, 'data', sample, 'Pupil', modality))
                 # create columns of start/end timestamps
                 Blin['s_timestamp'] = dt_start + pd.to_timedelta(Blin['start_timestamp'], unit='S')
                 Blin['e_timestamp'] = Blin['s_timestamp'] + pd.to_timedelta(Blin['duration'], unit='S')
@@ -144,7 +88,7 @@ class Preprocessor(object):
 
             elif modality == 'fixations.csv':
 
-                Fixa = pd.read_csv(join(self.src_dir, sample, 'Pupil', modality))
+                Fixa = pd.read_csv(join(self.src_dir, 'data', sample, 'Pupil', modality))
                 # create columns of start/end timestamps
                 Fixa['s_timestamp'] = dt_start + pd.to_timedelta(Fixa['start_timestamp'], unit='S')
                 Fixa['e_timestamp'] = Fixa['s_timestamp'] + pd.to_timedelta(Fixa['duration'], unit='ms')
@@ -170,7 +114,7 @@ class Preprocessor(object):
                 #resampled_Fixa.fillna(method='ffill', inplace=True)
 
             elif modality == 'surface_events.csv':
-                Surf = pd.read_csv(join(self.src_dir, sample, 'Pupil', modality))
+                Surf = pd.read_csv(join(self.src_dir, 'data', sample, 'Pupil', modality))
                 # create columns of start/end timestamps
                 Surf['world_timestamp'] = dt_start + pd.to_timedelta(Surf['world_timestamp'], unit='S')
 
@@ -193,9 +137,10 @@ class Preprocessor(object):
                 resampled_Surf = pd.concat([resampled_Surf, interval_df3], ignore_index=True)
 
                 resampled_Surf.set_index('new_timestamp', inplace=True)
-
-            elif modality == 'annotations.csv':
-                Anno = pd.read_csv(join(self.src_dir, sample, 'Pupil', modality))
+                
+            # Load GTs
+            elif modality == "Annotations.xlsx":
+                Anno = pd.read_excel(join(self.src_dir, modality), sheet_name=sample, engine='openpyxl')
                 # create columns of start/end timestamps
                 Anno['timestamp'] = dt_start + pd.to_timedelta(Anno['timestamp'], unit='S')
 
@@ -226,18 +171,23 @@ class Preprocessor(object):
         #resampled_data = resampled_Head.merge(resampled_Fixa, how='left', left_index=True, right_index=True)
         #resampled_data = resampled_data.merge(resampled_Blin, how='left', left_index=True, right_index=True)
         # Use "reduce" function
-        resampled_data = resampled_Head.merge(resampled_Fixa, on='new_timestamp').merge(resampled_Blin, on='new_timestamp')
+        resampled_data = resampled_Head.merge(resampled_Fixa, on='new_timestamp').merge(resampled_Blin, on='new_timestamp').merge(resampled_Surf, on='new_timestamp').merge(resampled_Anno, on='new_timestamp')
 
         return resampled_data
 
 
 if __name__ == "__main__":
 
-    OUTPUT_TYPE = "csv"  # "csv" or "pkl"
-    data_dir = r"/Users/zhuangzhuangdai/Downloads/dataset_Annotations"
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--type', type=str, default='csv', choices=['csv', 'pkl'], required=False, help='specify pre-processed data format: "csv" or "pkl"')
+
+    args = parser.parse_args()
+
+    OUTPUT_TYPE = args.type  # "csv" or "pkl"
+    data_dir = r"/home/CAMPUS/daiz1/repo/WALI-HRI/dataset"
 
     if OUTPUT_TYPE == "csv":
-        # output one sample .csv for example
+        # output two sample .csv for example
         prep = Preprocessor(sheets_to_load=['GBSIOT_07B', 'GBSIOT_07D'], src_dir=data_dir)
 
         for k, v in prep.data.items():
@@ -247,7 +197,7 @@ if __name__ == "__main__":
             prep.data[k].to_csv(join(csv_dir, k + ".csv"))
 
     elif OUTPUT_TYPE == "pkl":
-        sheet_lst = [item for item in os.listdir(data_dir) if not item.startswith(".")]
+        sheet_lst = [item for item in os.listdir(join(data_dir, 'data')) if not item.startswith(".")]
 
         prep = Preprocessor(sheets_to_load=sheet_lst, src_dir=data_dir)
 
