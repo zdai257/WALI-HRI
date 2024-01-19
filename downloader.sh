69e1b903cbc4d110f3c966fca37c51a58d877f15
Zhuangzhuang Dai <daiz1@mailhub.aston.ac.uk>
2024-01-19 14:09:23 +0000

    Use gdown for sample download, instead of wget

--- downloader.sh
+++ downloader.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1lxS4fjqs31SIq5al9KxFSRbs0Vqq8ti4' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1lxS4fjqs31SIq5al9KxFSRbs0Vqq8ti4" -O WALI-HRI_sample.zip && rm -rf /tmp/cookies.txt
+file_id="1MY_9yI43AsNtO2GlW5x7FeaDt06z6-JS"
+
+# Install gdown if not already installed
+if ! command -v gdown &> /dev/null; then
+    echo "Installing gdown..."
+    python -m pip install gdown
+fi
+
+echo "Downloading the file..."
+gdown --id "$file_id"
+echo "Download complete!"
